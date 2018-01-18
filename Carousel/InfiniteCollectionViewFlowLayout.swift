f1e5ffd91cee831890e5b0dc8f13a9b29e426689
Fellipe Calleia <fellipe@calleia.com.br>
2018-01-18 14:22:11 -0200

    Add cell snapping

--- Carousel/InfiniteCollectionViewFlowLayout.swift
+++ Carousel/InfiniteCollectionViewFlowLayout.swift
@@ -13,8 +13,15 @@ class InfiniteCollectionViewFlowLayout: UICollectionViewFlowLayout {
     let standardItemAlpha = CGFloat(0.5)
     let standardItemScale = CGFloat(0.5)
     
+    var isSetup = false
+    
     override func prepare() {
         super.prepare()
+        
+        if isSetup == false {
+            setupCollectionView()
+            isSetup = true
+        }
     }
     
     func changeLayoutAttributes(_ attributes: UICollectionViewLayoutAttributes) {
@@ -50,5 +57,32 @@ class InfiniteCollectionViewFlowLayout: UICollectionViewFlowLayout {
     
     override open func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
         return true
+    }
+    
+    
+    // MARK: - Cell Snapping
+    
+    func setupCollectionView() {
+        self.collectionView!.decelerationRate = UIScrollViewDecelerationRateFast
+        
+        let collectionSize = collectionView!.bounds.size
+        let yInset = (collectionSize.height - self.itemSize.height) / 2
+        let xInset = (collectionSize.width - self.itemSize.width) / 2
+        
+        self.sectionInset = UIEdgeInsetsMake(yInset, xInset, yInset, xInset)
+    }
+    
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+        
+        let layoutAttributes = self.layoutAttributesForElements(in: collectionView!.bounds)
+        
+        let center = collectionView!.bounds.size.height / 2
+        let proposedContentOffsetCenterOrigin = proposedContentOffset.y + center
+        
+        let closest = layoutAttributes!.sorted { abs($0.center.y - proposedContentOffsetCenterOrigin) < abs($1.center.y - proposedContentOffsetCenterOrigin) }.first ?? UICollectionViewLayoutAttributes()
+        
+        let targetContentOffset = CGPoint(x: proposedContentOffset.x, y: floor(closest.center.y - center))
+        
+        return targetContentOffset
     }
 }
